021c97cc24d460eef7e0f34ccae585edd595530f
pclin87 <73913675+pclin87@users.noreply.github.com>
2022-01-10 13:55:47 -1000

    vgb

---
+++ APM/src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform{
+    transform(value: string, charactor: string): string {
+        return value.replace(charactor, ' ')
+    }
+}
