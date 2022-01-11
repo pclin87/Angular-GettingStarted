b655640bb77cf397a16df1572e7079546d5162b5
pclin87 <73913675+pclin87@users.noreply.github.com>
2022-01-11 13:50:24 -1000

    module 12

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,18 @@ import { WelcomeComponent } from './home/welcome.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { 
+        path: 'products/:id', 
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent 
+      },
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
--- APM/src/app/products/product-detail.guard.ts
+++ APM/src/app/products/product-detail.guard.ts
@@ -12,13 +12,12 @@ export class ProductDetailGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const id = Number(route.paramMap.get('id'));
-    if (isNaN(id) || id < 1) {
-      alert('Invalid product id');
-      this.router.navigate(['/products']);
-      return false;
+      const id = Number(route.paramMap.get('id'))
+      if(isNaN(id) || id < 1){
+        alert('Invalaid product id')
+        this.router.navigate(['/products'])
+        return false
+      }
+      return true;
     }
-    return true;
-  }
-
 }
